88a1008cc70fb07b4f580221dedce5547f85607c
becerra2906 <55593696+becerra2906@users.noreply.github.com>
2020-06-17 09:05:05 -0500

    Health potion project for The Python Bible Course
    Adds health points to a character and prints it using random randint function. It also uses a difficulty variable to decrease the value of the health points added to the character.

---
+++ health_potion.py
@@ -0,0 +1,19 @@
+### By Alejandro Becerra
+### written as part of The Python Bible course
+### as a small introductory project to python
+
+import random 
+
+health = 50
+
+difficulty = 1
+
+potion_health = 
+   int(random.randint(25,50) / difficulty)
+    
+health = health + potion_health
+
+print(health)
+
+
+
