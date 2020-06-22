3166544ff9c800024386c4631e1f0d7c97cded6f
becerra2906 <55593696+becerra2906@users.noreply.github.com>
2020-06-22 14:14:14 -0500

    V1 of The Python Bible Course (Unfinished)
    Currently saves user inputs of name, age, city and hobby as variables. Could print them but has not concatenated the different inputs as an output phrase.

---
+++ hello_you.py
@@ -0,0 +1,23 @@
+# By Alejandro Becerra 
+# done as part of The Python Bible Udemy Course
+
+
+#ask user a name
+
+name = input("What's your name?: ")
+
+#ask user for age
+
+age = input("What's your age?: ")
+
+#ask user for city
+
+city = input("What city do you live in?: ")
+
+#ask user what they enjoy 
+
+love = input("What do you love doing?: ")
+
+#Create ouput 
+
+ 
