415934dcdd4dabc29b9581bbde31f1f343bb3087
Alejandro Becerra <alejobecerra1995@gmail.com>
2020-07-07 09:18:37 -0500

    Create tell_us_about_yourself.py

---
+++ tell_us_about_yourself.py
@@ -0,0 +1,21 @@
+#done for The python bible udemy course
+
+# Create a variable called name and set it equal to your name
+
+
+# Create a variable called age and set it equal to your age
+
+
+# Create a variable called output, and use {} symbols and the format() function to 
+# make it contain a string like the example text in the description
+
+
+# Finally, use the print() function to print the output.
+
+#Example output: "My name is Sarah and I am 46 years old"
+
+name = "Alejandro"
+age = 25
+
+output = "My name is {} and I am {} years old".format(str(name), str(age))
+print(output)
