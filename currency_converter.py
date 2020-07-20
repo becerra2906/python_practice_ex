9875a0dbec38a079136404d471852773c398e8f6
Alejandro Becerra <alejobecerra1995@gmail.com>
2020-07-20 08:48:25 -0500

    Added Dollar converter to pesos
    Added dollar convertor to cop, with input for currency selector, and added several options to accept inputed currency.

--- currency_converter.py
+++ currency_converter.py
@@ -3,13 +3,36 @@
 #### This program will take an inputted ammount of 
 #### Colombian pesos (COP) and transform it to an ammount of USD
 
-pesos = input("¿Cuántos pesos colombianos tienes? ")
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / float(valor_dolar)
-dolares = round(dolares, 2)
-dolares = str(dolares)
-confirmation_message = "Tienes $ {} dolares".format(dolares)
-print(confirmation_message)
+init = input("¿Qué moneda quieres convertir? ")
+
+monedas = ("dolares", "pesos colombianos", "Dolares", "dolares ", "Dolares ", "Pesos Colombianos", "pesos ","pesos", "Pesos Colombianos ", "pesos colombianos ")
+
+if init in ("pesos colombianos","Pesos Colombianos", "pesos ","pesos", "Pesos Colombianos ", "pesos colombianos "): 
+
+    pesos = input("¿Cuántos pesos colombianos tienes? ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / float(valor_dolar)
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    confirmation_message = "Tienes $ {} dolares".format(dolares)
+    print(confirmation_message)
+
+if init in ("dolares", "Dolares", "dolares ", "Dolares "): 
+
+    dolares = input("¿Cuántos dolares tienes? ")
+    dolares = float(dolares)
+    valor_peso = 0.27
+    pesos = (dolares / float(valor_peso))*1000
+    ### pesos = round(pesos, 2)
+    pesos = str(pesos)
+    confirmation_message = "Tienes $ {} pesos".format(pesos)
+    print(confirmation_message)
+
+
+if init not in monedas : 
+    print ("En este momento no soportamos esa moneda, vuelve a correr el programa e intenta con Dolares o Pesos Colombianos")
+
+
 
 
