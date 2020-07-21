2791a0077944afeb5143202c4aed7656dc9e7cc1
Alejandro Becerra <alejobecerra1995@gmail.com>
2020-07-21 17:59:32 -0500

    Update condicionales.py
    Exercise 2 of the lecture.

--- condicionales.py
+++ condicionales.py
@@ -1,7 +1,15 @@
 ### done as part of Platzi's basic python course
 
-edad = int(input("Escribe tu edad: "))
-if edad > 17: 
-    print("Eres mayor de edad.")
-else:  
-    print ("Eres menor de edad.")
+### edad = int(input("Escribe tu edad: "))
+### if edad > 17: 
+###    print("Eres mayor de edad.")
+### else:  
+###    print ("Eres menor de edad.")
+
+numero = int(input('Escribe un número: ')) 
+if numero > 5: 
+    print('Es mayor a 5.')
+if numero < 5: 
+    print('Es menor a 5.')
+if numero == 5:
+    print ('Tu numero es 5.')
