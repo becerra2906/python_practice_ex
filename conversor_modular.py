118009cab0cef0827b43723950b2fe38a96e4d5e
Alejandro Becerra <alejobecerra1995@gmail.com>
2020-07-22 08:22:02 -0500

    Create conversor_modular.py
    start of function transformation for the currency converter to a function based program

---
+++ conversor_modular.py
@@ -0,0 +1,37 @@
+#init = input("¿Qué moneda quieres convertir? ")
+
+#monedas = ("dolares", "pesos colombianos", "Dolares", "dolares ", "Dolares ", "Pesos Colombianos", "pesos ","pesos", "Pesos Colombianos ", "pesos colombianos ")
+
+#if init in ("pesos colombianos","Pesos Colombianos", "pesos ","pesos", "Pesos Colombianos ", "pesos colombianos "): 
+
+    #pesos = input("¿Cuántos pesos colombianos tienes? ")
+    #pesos = float(pesos)
+    #valor_dolar = 3875
+    #dolares = pesos / float(valor_dolar)
+    #dolares = round(dolares, 2)
+    #dolares = str(dolares)
+    #confirmation_message = "Tienes $ {} dolares".format(dolares)
+    #print(confirmation_message)
+
+#if init in ("dolares", "Dolares", "dolares ", "Dolares "): 
+
+    #dolares = input("¿Cuántos dolares tienes? ")
+    #dolares = float(dolares)
+    #valor_peso = 0.27
+    #pesos = (dolares / float(valor_peso))*1000
+    ### pesos = round(pesos, 2)
+    #pesos = str(pesos)
+    #confirmation_message = "Tienes $ {} pesos".format(pesos)
+    #print(confirmation_message)
+
+
+#if init not in monedas : 
+    #print ("En este momento no soportamos esa moneda, vuelve a correr el programa e intenta con Dolares o Pesos Colombianos")
+
+
+def suma(a,b):
+    print('Se suman dos numeros: ')
+    resultado = a + b
+    return resultado
+sumatoria = suma(1,4)
+print(sumatoria)
