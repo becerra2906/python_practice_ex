f9a78bd37bf8733bf7d211bbbb6e363330e6bca3
Alejandro Becerra <alejobecerra1995@gmail.com>
2020-07-22 08:11:11 -0500

    Create funciones.py
    Exercise done as part of Platzi´s basic python course to learn how to use functions and elifs

---
+++ funciones.py
@@ -0,0 +1,26 @@
+# part of platzi´s python basic course
+
+# def imprimir_mensaje():
+    # print("Mensaje Especial: ")
+    # print("Estoy aprendiendo a usar funciones!")
+
+# imprimir_mensaje()
+
+def conversacion(mensaje):
+    print('Hola ')
+    print('Como estás? ')
+    print(mensaje)
+    print('Adiós')
+
+
+opcion = int(input("Elige una opción (1,2,3): "))
+
+if opcion == 1: 
+    conversacion(mensaje = 'Elegiste la ópcion 1.')
+elif opcion == 2:
+      conversacion(mensaje = 'Elegiste la ópcion 2.')
+elif opcion == 3:
+     conversacion(mensaje = 'Elegiste la ópcion 3.')
+else: 
+    print('Elige una opción valida. ')
+
